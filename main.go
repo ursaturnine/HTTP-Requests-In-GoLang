aaa6049339502d4fbe5e8574775a21cad9742531
Tyrah <tyrah96@gmail.com>
2023-02-24 17:49:42 -0800

    implementing the read function in body property of response object

--- main.go
+++ main.go
@@ -18,5 +18,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(res)
+	// make a byte slice with n number of empty elements in side of it
+	bs := make([]byte, 99999)
+	// takes body and puts inside byte slice
+	res.Body.Read(bs)
+	// this displays response body as a string (response body is the HTML of the Google home page)
+	fmt.Println(string(bs))
 }
